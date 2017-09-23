d17d2f7c78a54428033c43d6d73a75ecd99496fc
Luke Robinson <robinsonlm@live.co.uk>
2017-09-23 11:16:24 +0100

    Implemented the coin pickup and score

---
+++ Assets/Scripts/Game Dynamics/CoinPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+	private ScoreManager scoreManager;
+
+	// Use this for initialization
+	void Start () {
+		scoreManager = ScoreManager.instance;
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+        if (other.isTrigger && other.gameObject.CompareTag("Player")) {
+			Debug.Log("Collided with player!");
+			scoreManager.IncreaseScore(10f);
+			Destroy(gameObject);
+		}
+    }
+}
---
+++ Assets/Scripts/Game Dynamics/ScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static ScoreManager instance = null;
+
+	public float score;
+	[SerializeField]
+	private Text scoreText;
+
+	void Awake() {
+		if(instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+
+		 DontDestroyOnLoad(gameObject);
+		 Reset();
+	}
+
+	public void Reset() {
+		score = 0f;
+		UpdateScoreText();
+	}
+
+	public void IncreaseScore(float amount) {
+		score += amount;
+		UpdateScoreText();
+	}
+
+	private void UpdateScoreText() {
+		scoreText.text = "Score: " + score;
+	}
+}
